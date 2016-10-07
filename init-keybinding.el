39e20a7acc6937a915c676bacedcd6edb0adbb4f
tracyone <tracyone@live.cn>
2016-10-07 21:16:16 +0800

    use use-package.

--- init-keybinding.el
+++ init-keybinding.el
@@ -1,74 +1,24 @@
 
-(global-set-key "\C-x\ \C-r" 'recentf-open-files)
 
 ;;smex simlar to ctrlp
-(global-set-key "\C-s" 'swiper)
-(global-set-key (kbd "C-c C-r") 'ivy-resume)
-(global-set-key (kbd "<f6>") 'ivy-resume)
-(global-set-key (kbd "M-x") 'counsel-M-x)
-(global-set-key (kbd "C-x C-f") 'counsel-find-file)
-(global-set-key (kbd "<f1> f") 'counsel-describe-function)
-(global-set-key (kbd "<f1> v") 'counsel-describe-variable)
-(global-set-key (kbd "<f1> l") 'counsel-load-library)
-;;(global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
-;;(global-set-key (kbd "<f2> u") 'counsel-unicode-char)
-(global-set-key (kbd "C-c g") 'counsel-git)
-(global-set-key (kbd "C-c j") 'counsel-git-grep)
-(global-set-key (kbd "C-c k") 'counsel-ag)
-(global-set-key (kbd "C-x l") 'counsel-locate)
-(global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
 
 ;;open emacs config file
 (global-set-key (kbd "<f2>") 'open-my-init-file)
-(evil-leader/set-key "vc" 'open-my-init-file)
-(evil-leader/set-key
-  "vc" 'open-my-init-file
-  "vv" 'helm-do-ag-project-root
-  "bk" 'kill-buffer
-  "cc" 'comment-region
-  "cu" 'comment-or-uncomment-region
-  "bb" 'switch-to-buffer
-  "fr" 'recentf-open-files
-  "g" 'counsel-git
-  "p" 'counsel-find-file
-  )
 
 (global-set-key (kbd "<f1>") 'reveal-in-osx-finder)
-(global-set-key (kbd "C-M-\\") 'indent-region-or-buffer)
+
 ;;补全路径
 (global-set-key (kbd "s-/") 'hippie-expand)
 
 (define-key dired-mode-map (kbd "RET") 'dired-find-alternate-file)
 
-;;
-(global-set-key (kbd "M-s i") 'counsel-imenu)
-
 ;;正则递归搜索
 (global-set-key (kbd "M-s o") 'occur-dwim)
 
-;;expand region
-(global-set-key (kbd "C-=") 'er/expand-region)
-
-;;iedit
-(global-set-key (kbd "M-s e") 'iedit-mode)
-
-(global-set-key (kbd "C-c p s") 'helm-do-ag-project-root)
-
-;;用c-n和c-p来选择补全
-(with-eval-after-load 'company
-  (define-key company-active-map (kbd "M-n") nil)
-  (define-key company-active-map (kbd "M-p") nil)
-  (define-key company-active-map (kbd "C-n") #'company-select-next)
-    (define-key company-active-map (kbd "C-p") #'company-select-previous)
-    )
 
 ;;readline 快捷键设定
 (global-set-key (kbd "C-w") 'backward-kill-word)
 
-(global-set-key (kbd "<f3>") 'magit-status)
-
-(evilem-define (kbd "W") 'evil-forward-word-begin)
-(evilem-define (kbd "B") 'evil-backward-WORD-begin)
 
 (provide 'init-keybinding)
 
--- init-packages.el
+++ init-packages.el
@@ -43,6 +43,7 @@
 			    window-numbering
 			    powerline
 			    which-key;;实时显示按键提示
+			    use-package
                             ) "Default packages" )
 
 (defun tracyone/packages-installed-p ()
@@ -57,65 +58,170 @@
     (when (not (package-installed-p pkg))
       (package-install pkg))))
 
+;; plugin config and load ===============================
 
-(global-hungry-delete-mode)
+;;Plugin setting
+(use-package hungry-delete
+  :config
+  (global-hungry-delete-mode)
+  )
 
-(smartparens-global-mode t)
-;;一些情况下不要出现两个单引号
-(sp-local-pair 'emacs-lisp-mode "'" nil :actions nil)
+(use-package smartparens
+  :config
+  (smartparens-global-mode t)
+  ;;一些情况下不要出现两个单引号
+  (sp-local-pair 'emacs-lisp-mode "'" nil :actions nil)
+  (sp-local-pair 'lisp-interaction-mode "'" nil :actions nil)
+  )
 
-(sp-local-pair 'lisp-interaction-mode "'" nil :actions nil)
+
+(use-package ivy
+  :bind (
+	 ("C-c C-r" . ivy-resume)
+	 )
+  :config
+  (ivy-mode 1)
+  (setq ivy-use-virtual-buffers t)
+  )
 
 
-(ivy-mode 1)
-(setq ivy-use-virtual-buffers t)
-
-(global-company-mode t)
+(use-package company
+  :config
+  (global-company-mode t)
+  ;;用c-n和c-p来选择补全
+  (define-key company-active-map (kbd "M-n") nil)
+  (define-key company-active-map (kbd "M-p") nil)
+  (define-key company-active-map (kbd "C-n") #'company-select-next)
+  (define-key company-active-map (kbd "C-p") #'company-select-previous)
+  )
 
 (load-theme 'molokai t)
 
-(require 'popwin)
-(popwin-mode t)
+(use-package popwin
+  :config
+  (popwin-mode t)
+  )
 
 ;;支持最近文件
-(require 'recentf)
-(recentf-mode 1)
+(use-package recentf
+  :bind (
+	 ("\C-x\ \C-r" . recentf-open-files)
+	 )
+  :config
+  (recentf-mode 1)
+  )
 
-;;Plugin setting
-(require 'hungry-delete)
-(global-hungry-delete-mode)
 
-(require 'smex)
+(use-package smex
+  :bind (
+	 ("\C-s" . swiper)
+	 )
+  )
 
-(require 'dired)
-(require 'dired-x)
-;;(global-set-key (kbd "M-x") 'smex)
+(use-package dired)
 
-;(global-flycheck-mode)
+(use-package dired-x)
 
-(require 'yasnippet)
-(yas-global-mode 1)
-(add-hook 'prog-mode-hook #'yas-minor-mode)
 
-(evil-mode 1)
-(setq evil-want-C-u-scroll 1)
+(use-package yasnippet
+  :config
+  (yas-global-mode 1)
+  (add-hook 'prog-mode-hook #'yas-minor-mode)
+  )
 
-(global-evil-leader-mode)
-(evil-leader/set-leader "SPC")
+(use-package evil
+  :config
+  (evil-mode 1)
+  (setq evil-want-C-u-scroll 1)
+  )
 
-(window-numbering-mode)
+(use-package evil-leader
+  :config
+  (global-evil-leader-mode)
+  (evil-leader/set-leader "SPC")
+  (evil-leader/set-key "vc" 'open-my-init-file)
+  (evil-leader/set-key
+    "vc" 'open-my-init-file
+    "vv" 'helm-do-ag-project-root
+    "bk" 'kill-buffer
+    "cc" 'comment-region
+    "cu" 'comment-or-uncomment-region
+    "bb" 'switch-to-buffer
+    "fr" 'recentf-open-files
+    "g" 'counsel-git
+    "p" 'counsel-find-file
+    )
+  )
 
-(require 'powerline)
+(use-package window-numbering
+  :config
+  (window-numbering-mode)
+  )
 
-(powerline-default-theme)
+(use-package powerline
+  :config
+  (powerline-default-theme)
+  )
 
-(require 'evil-surround)
-(global-evil-surround-mode)
+(use-package evil-surround
+  :config
+  (global-evil-surround-mode)
+  )
 
-(which-key-mode 1)
-(setq which-key-side-window-location 'right)
+(use-package which-key
+  :config
+  (which-key-mode 1)
+  (setq which-key-side-window-location 'right)
+  )
 
-(require 'evil-easymotion)
+(use-package evil-easymotion
+  :config
+  (evilem-define (kbd "W") 'evil-forward-word-begin)
+  (evilem-define (kbd "B") 'evil-backward-WORD-begin)
+  )
+
+(use-package counsel
+  :bind (
+	  ( "M-x" . counsel-M-x)
+	  ( "C-x C-f" . counsel-find-file)
+	  ( "<f1> f" . counsel-describe-function)
+	  ("<f1> v" . counsel-describe-variable)
+	  ("<f1> l" . counsel-load-library)
+	  ("C-c g" . counsel-git)
+	  ("C-c j" . counsel-git-grep)
+	  ("C-c k" . counsel-ag)
+	  ("C-x l" . counsel-locate)
+	  ("C-S-o" . counsel-rhythmbox)
+	  ("M-s i" . counsel-imenu)
+	  )
+  )
+
+(use-package expand-region
+  :bind (
+	 ;;expand region
+	 ("C-=" . er/expand-region)
+	 )
+  )
+
+(use-package iedit
+  :bind (
+	 ;;iedit
+	 ("M-s e" . iedit-mode)
+	 )
+  )
+
+(use-package helm-ag
+  :bind (
+	 ("C-c p s" . helm-do-ag-project-root)
+	 )
+  )
+
+(use-package magit
+  :bind (
+	 ("<f3>" . magit-status)
+	 )
+
+  )
 
 (provide 'init-packages)
 
